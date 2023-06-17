49c663b92a06982f1219c8b85ef9c7bfef309e8d
Shraddha Hukkeri <113444373+Shraddha3122@users.noreply.github.com>
2023-06-17 06:53:39 +0530

    PHP Added

---
+++ Fibonacci series.php
@@ -0,0 +1,21 @@
+<?php  
+
+function Fibonacci($number)
+{
+       if ($number == 0)
+        return 0;    
+    else if ($number == 1)
+        return 1;    
+      
+    // Recursive Call to get numbers
+    else
+        return (Fibonacci($number-1) + 
+                Fibonacci($number-2));
+}
+  
+$number = 10;
+for ($counter = 0; $counter < $number; $counter++)
+{  
+    echo Fibonacci($counter),' ';
+}
+?>
